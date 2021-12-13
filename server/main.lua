79a030ee3e2448b2c8f1d0e0a9578729fa652d6e
KYLE S <79766794+418Gaming@users.noreply.github.com>
2021-12-13 16:07:51 -0500

    Added Weapon Serial Number Scratch

--- server/main.lua
+++ server/main.lua
@@ -735,3 +735,29 @@ end)
 QBCore.Functions.CreateUseableItem('marksmanrifle_luxuryfinish', function(source, item)
     TriggerClientEvent('weapons:client:EquipAttachment', source, item, 'luxuryfinish')
 end)
+
+--Serial Number Scratch
+
+QBCore.Functions.CreateUseableItem("metalfile", function(source, item)
+    local Player = QBCore.Functions.GetPlayer(source)
+    if Player.Functions.RemoveItem(item.name, 1, item.slot) then
+        TriggerClientEvent('qb-weapons:client:scratch', source)
+    end
+end)
+
+RegisterNetEvent('qb-weapons:server:scratch', function(weapon, weaponammo)
+    local Player = QBCore.Functions.GetPlayer(source)
+    local weaponInfo = QBCore.Shared.Weapons[weapon]
+    local weaponName = weaponInfo["name"]
+    if weaponInfo then
+        weaponInfo = Player.Functions.GetItemByName(weaponName)
+        if weaponInfo then
+            local info = {
+                ammo = weaponammo,
+                serie="xxxxxx"
+            }
+            Player.Functions.RemoveItem(weaponName, 1)
+            Player.Functions.AddItem(weaponName, 1, weaponInfo.slot, info)
+        end
+    end
+end)
